5df3c14a8e1735c1572a9468e02719cb67fd0c70
jyoung0930 <jaronyoung1@gmail.com>
2022-11-04 09:08:40 -0500

    Continued Development
    Finished adding mobile and tablet layouts.

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
-
   loop: true,
 
-  // If we need pagination
   pagination: {
     el: ".swiper-pagination",
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
